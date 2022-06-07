09f9ba4372aa5a5f295cf5fcb323c4fe1aa6543b
JoC7S <106079368+JoC7S@users.noreply.github.com>
2022-06-07 10:04:06 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $EUID -ne 0 ]; then
+	echo " "
+	echo "$(tput setaf 3)[$(tput setaf 1)*$(tput setaf 3)]Ejecuta el script como root"
+	exit 1
+fi
+
+chmod -x exallports
+cp exallports /usr/bin/
+
+echo "$(tput setaf 3)[$(tput setaf 1)*$(tput setaf 3)]$(tput setaf 6)Completado"
